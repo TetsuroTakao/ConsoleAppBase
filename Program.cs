389987d3b60b244fb34951a003ff4ce143be4af5
Tetsuro Takao <tetsuro.takao@processtune.com>
2025-06-18 10:14:04 +0900

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿#region using
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Sinks.File;
+#endregion
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+ILoggerFactory factory = LoggerFactory.Create(builder =>
+{
+    builder.AddConsole();
+    builder.AddSerilog(Log.Logger, dispose: true);
+});
+Microsoft.Extensions.Logging.ILogger logger = factory.CreateLogger<Program>();
+builder.Configuration.Sources.Clear();
+IHostEnvironment env = builder.Environment;
+builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+    .AddUserSecrets<Program>(optional: true, reloadOnChange: true);
+logger.LogInformation("Hello, World!");
+using IHost host = builder.Build();
